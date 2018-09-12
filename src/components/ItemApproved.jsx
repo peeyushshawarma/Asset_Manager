a2609856e2d77cfa18573b390d357d9b1d646cb0
peeyush Sharma <peeyushs37@gmail.com>
2018-09-12 19:09:49 +0530

    Surrender added

--- src/components/ItemApproved.jsx
+++ src/components/ItemApproved.jsx
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
+import {approvedRef} from '../firebase';
 
 class ItemApproved extends Component{
+
+  removeasset(approvedReqKey){
+    if(window.confirm('Are you sure?'))
+    approvedRef.child(approvedReqKey).remove();
+  }
   render(){
-    //console.log('this.props', this.props);
-    const {asset} = this.props.eachApproval;
+    console.log('this.props', this.props);
+    const {asset,approvedReqKey} = this.props.eachApproval;
     const {email}= this.props.user;
     return(
       <div>
         <strong>{asset}</strong>
         &nbsp;
         <span>
-          <button className='btn btn-link btn-xs' style={{color:'#27AE60'}}>
+          <button className='btn btn-link btn-xs' style={{color:'#27AE60'}} onClick={()=>this.removeasset(approvedReqKey)}>
             <u>surrender</u>
           </button>
         </span>
